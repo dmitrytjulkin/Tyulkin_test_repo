c0f7468e9ed1efaa950e9c67d4eee791f7b7971a
Tyulkin Dmitry <Dmitrytjulkin@yandex.ru>
2026-02-17 16:42:24 +0300

    added r2r_dac.py and corrected bugs in 8-bit-dac-manual.py

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -1,35 +1,38 @@
 import RPi.GPIO as GPIO
-import time
 
-GPIO.setmode(GPIO.BCM)
+dac_pins = [22, 27, 17, 26, 25, 21, 20, 16]
 
-dac_bits = [22, 27, 17, 26, 25, 21, 20, 16]
-GPIO.setup(dac_bits, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)          
+GPIO.setup(dac_pins, GPIO.OUT)  
 
-dynamic_range = 3.3
-
+dynamic_range = 3.18
 def voltage_to_number(voltage):
-        if not (0.0 <= voltage <= dynamic_range):
-            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} B)")
-            print("Устанавливаем 0.0 В")
-            return 0
+    if not (0.0 <= voltage <= dynamic_range):
+        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} В)")
+        print("Устанавлниваем 0.0 В")
+        return 0
 
-        return int(voltage / dynamic_range * 255)
+    return int(voltage / dynamic_range * 255)
+
 
 def number_to_dac(number):
-    bin_num = [int(element) for element in bin(number)[2:].zfill(8)]
-    GPIO.output(dac_bits, bin_num)
 
+    for i in range(8):
+        
+        bit_value = (number >> i) & 13
+        
+        GPIO.output(dac_pins[i], bit_value)
 try:
     while True:
         try:
             voltage = float(input("Введите напряжение в Вольтах: "))
             number = voltage_to_number(voltage)
             number_to_dac(number)
-        
+
         except ValueError:
-            print("Вы ввели не число. Попробуйте еще раз \n")
+            print("Вы ввели не число. Попробуйте ещё раз\n")
 
 finally:
-    GPIO.output(dac_bits, 0)
+    GPIO.output(dac_pins, 0)
     GPIO.cleanup()
+
---
+++ get-dac/r2r_dac.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+class R2R_DAC:
+    def __init__(self, gpio_bits, dynamic_range, verbose = False):
+        self.gpio_bits = gpio_bits
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
+    
+    def deinit(self):
+        GPIO.output(self.gpio_bits, 0)
+        GPIO.cleanup()
+
+    def set_number(self, number):
+        for i in range(8):
+           bit_value = (number >> i) & 1
+           GPIO.output(self.gpio_bits[i], bit_value)     
+        if self.verbose:
+            print(f"Установленно число: {number}")
+
+    def set_voltage(self, voltage):
+        max_number = 2**8 - 1
+        number = int(voltage * max_number / self.dynamic_range)
+        number = max(0, min(number, max_number))
+        self.set_number(number)
+        if self.verbose:
+            print(f"Установленное напряжение: {voltage:.2f}")
+
+if __name__ == "__main__":
+    dac = None
+    try:
+        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
+
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в Вольтах: "))
+                dac.set_voltage(voltage)
+            
+            except ValueError:
+                print("Вы ввели не число. Попробуйте еще раз\n")
+    
+    finally:
+        dac.deinit()
+
